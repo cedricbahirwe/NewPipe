d594a79a0ca3b4584d837bdb902be2db5fa409d3
Cédric Bahirwe <cedricaganzebahirwe@gmail.com>
2024-11-16 00:00:27 +0200

    Initial Commit

---
+++ NewPipeTests/NewPipeTests.swift
@@ -0,0 +1,17 @@
+//
+//  NewPipeTests.swift
+//  NewPipeTests
+//
+//  Created by Cédric Bahirwe on 16/11/2024.
+//
+
+import Testing
+@testable import NewPipe
+
+struct NewPipeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
